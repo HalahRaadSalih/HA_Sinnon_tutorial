7d999e6491becd931d7f1cfd18929311b4c47e6a
Halah Al-Shaikhly <Halah.alshaikhly@gmail.com>
2016-11-30 17:49:54 -0800

    add test to demp spies with funtions

--- test/some_test.js
+++ test/some_test.js
@@ -11,3 +11,22 @@ describe('Spies', () => {
     console.log(spy.firstCall.args);
   });
 });
+
+describe('Spies', () => {
+  it('should demo using spies with functions', () => {
+    var user = {
+      setName: function(name){
+        this.name = name;
+      }
+    }
+
+    var setNameSpy = sinon.spy(user, 'setName');
+
+    user.setName('Darth Vader');
+    user.setName('Darth ');
+
+    console.log(setNameSpy.callCount);
+    expect(setNameSpy.callCount).to.equal(2);
+    setNameSpy.restore();
+  })
+});
